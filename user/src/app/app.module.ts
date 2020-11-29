b10e4a0cedb5025313369f5fa08359252b05413f
Emmanuel <emmanuel.maurice@gmail.com>
2020-11-29 19:48:03 +0100

    app.module

--- user/src/app/app.module.ts
+++ user/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ApiHttpInterceptor } from './api-http-interceptor';
 
+const routes: Routes = [
+  { path: 'connexion', component: ConnexionComponent }
+];
+
+ReactiveFormsModule
 @NgModule({
   declarations: [
     AppComponent,
     ConnexionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    {
+    provide: HTTP_INTERCEPTORS,    useClass: ApiHttpInterceptor,     multi: true
+    }
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
